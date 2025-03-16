522700736c062d8df27e1e2ab6004a48104963e4
Mikita Biahletski <steveefox@Mikitas-MacBookPro.local>
2025-03-16 20:12:47 +0100

    add proj

---
+++ CxjParallaxScrollCollectionExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CxjParallaxScrollCollectionExample
+//
+//  Created by Nikita Begletskiy on 16/03/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
